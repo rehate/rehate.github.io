474fb9679b472130605c79ac03a7af8ab5f9087e
JulianKov <yulian.kov@gmail.com>
2018-11-17 19:15:10 +0300

    fix: mobile scale

--- yt/scripts/script.js
+++ yt/scripts/script.js
@@ -1,19 +1,4 @@
-window.onload = function () {
-  hideAddressBar();
-  window.addEventListener("orientationchange", function () {
-      hideAddressBar();
-  }, false);
-}
-
-function hideAddressBar() {
-  setTimeout(function () {
-      document.body.style.height = window.outerHeight + 'px';
-      setTimeout(function () {
-          window.scrollTo(0, 1);
-      }, 1100);
-  }, 1000);
-  return false;
-}
+window.addEventListener("load", function() { window. scrollTo(0, 0); });
 
 const youtube = new YouTube;
 const ui = new UI;
